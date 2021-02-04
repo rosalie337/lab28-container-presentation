3bde7e4ffe5555080110ceb081e5d83a80b600f6
Rosalie Lee <sah-lee@Rosalies-MacBook-Pro.local>
2021-02-03 18:14:01 -0800

    added Article presentational component

--- src/components/presentational/Article.jsx
+++ src/components/presentational/Article.jsx
@@ -1,2 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+const Article = ({title, author, image}) => (
+    <figure>
+        <h3>{title}</h3>
+        <p>{author}</p>
+        <image src={image} />
+    </figure>
+);
+
+Article.PropTypes = {
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+};
+
+export default Article;
